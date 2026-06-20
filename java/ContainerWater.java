afb2b1e517e5695d4f562a689195dfba680280f0
kalaivcare <kalaiguna2909@gmail.com>
2026-06-20 10:09:28 +0530

    new added

---
+++ java/ContainerWater.java
@@ -0,0 +1,20 @@
+public class ContainerWater {
+    public static void main(String[] args){
+        int[] height= {1,8,6,2,5,4,8,3,7};
+        int maxArea=0;
+        int left=0;
+        int right=height.length-1;
+        while(left < right) {
+            int h=Math.min(height[left],height[right]);
+            int width=right-left;
+            int area=h*width;
+            maxArea=Math.max(maxArea,area);
+            if(height[left] < height[right]){
+                left++;
+            }else{
+                right--;
+            }
+        }
+        System.out.println(maxArea);
+    }
+}
---
+++ java/LongestPalindrome.java
@@ -0,0 +1,37 @@
+public class LongestPalindrome {
+    public static void main(String[] args){
+        String s = "babad";
+            String ans = "";
+
+            for (int i = 0; i < s.length(); i++) {
+                for (int j = i; j < s.length(); j++) {
+                    String sub = s.substring(i, j + 1);
+
+                    if (isPalindrome(sub) && sub.length() > ans.length()) {
+                        ans = sub;
+                    }
+                }
+            }
+
+           System.out.println(ans);
+        }
+
+           
+        public static boolean isPalindrome(String str) {
+            int left = 0;
+            int right = str.length() - 1;
+
+            while (left < right) {
+                if (str.charAt(left) != str.charAt(right)) {
+                    return false;
+                }
+                left++;
+                right--;
+            }
+
+            return true;
+        }
+}
+        
+        
+    
